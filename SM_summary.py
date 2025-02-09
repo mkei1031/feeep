7d9f32749ee507a016d952e434bc3a01123a56cd
mkei1031 <contact@qiao.jp>
2025-02-09 17:29:49 +0900

    Update SM_summary.py

--- SM_summary.py
+++ SM_summary.py
@@ -8,7 +8,7 @@ current_time = datetime.datetime.now()
 current_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
 
 #スペースマーケットの全データを読み込む
-url = https://github.com/mkei1031/feeep_sm/blob/main/all_SMrecords.csv
+url = 'https://github.com/mkei1031/feeep_sm/blob/main/all_SMrecords.csv'
 df = pd.read_csv(url, parse_dates=['実施日', '予約リクエスト日', '成約日'])
 
 #スペースマーケットのデータからキャンセルを省く
