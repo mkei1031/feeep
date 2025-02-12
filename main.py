2b43036b41f93bc53ad264d1cd02d6fd2969349f
mkei1031 <contact@qiao.jp>
2025-02-12 17:43:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.set_page_config(page_title="メインページ")
+st.set_page_config(page_title='FEEEPレポート')
 
-st.title("メインページ")
+# 他のページへのリンクを作成
+st.sidebar.title('目次')
+pages = {
+    'アプリサマリ',
+    'スペースマーケットサマリ'
+}
+
+page = st.sidebar.radio("ページを選んでください", list(pages.keys()))
+if page == 'アプリサマリ':
+    st.write("app_summary.py の内容が表示されます")
+    # app_summary.pyのインポートを行う場合もあります
+elif page == "スペースマーケットサマリ":
+    st.write("SM_summary.py の内容が表示されます")
+    # SM_summary.pyのインポートを行う場合もあります
+
 st.write("左のサイドバーからページを選択してください。")
