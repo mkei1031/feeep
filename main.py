8212ddc0c8b18e675cbb7468ad6bc044bc6cb5e2
mkei1031 <contact@qiao.jp>
2025-03-11 02:28:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ pages = {
     'スペイシーサマリ'
 }
 
-page = st.sidebar.radio("ページを選んでください", list(pages))
-if page == 'アプリサマリ':
-    st.write("app_summary.py の内容が表示されます")
-    # app_summary.pyのインポートを行う場合もあります
-elif page == "スペースマーケットサマリ":
-    st.write("SM_summary.py の内容が表示されます")
-    # SM_summary.pyのインポートを行う場合もあります
+#page = st.sidebar.radio("ページを選んでください", list(pages))
+# if page == 'アプリサマリ':
+#     st.write("app_summary.py の内容が表示されます")
+#     # app_summary.pyのインポートを行う場合もあります
+# elif page == "スペースマーケットサマリ":
+#     st.write("SM_summary.py の内容が表示されます")
+#     # SM_summary.pyのインポートを行う場合もあります
 
 st.write("左のサイドバーからページを選択してください。")
+st.wirte("all summary : アプリ・外部PFの統合データ")
+st.write("app summary : アプリデータ")
+st.write("IB summary : インスタベースデータ")
+st.write("SM summary : スペースマーケットデータ")
+st.write("SP summary : スペイシーデータ")
